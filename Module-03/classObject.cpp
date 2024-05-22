e6405953003688e65f2e3fa37e72e5f27570dc3c
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-23 00:14:12 +0600

    Add New Module

--- Module-03/classObject.cpp
+++ Module-03/classObject.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    double CGPA;
+};
 int main()
 {
-    
+    Student Taskin;
+    Taskin.roll = 10;
+    Taskin.CGPA = 3.74;
+    // we cant re assign a char directly
+    char temp[100] = "Taskin Ahmad Al Faruqe";
+    strcpy(Taskin.name, temp);
+
+    cout << Taskin.name;
     return 0;
 }
