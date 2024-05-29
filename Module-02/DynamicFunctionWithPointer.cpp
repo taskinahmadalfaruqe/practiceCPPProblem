9b2165f1ee6fbf3af478b81a4f2e8564acc755e5
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-30 01:31:11 +0600

    Add Dynamic Function

--- Module-02/DynamicFunctionWithPointer.cpp
+++ Module-02/DynamicFunctionWithPointer.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+int *fun()
+{
+    int *a = new int;
+    *a = 500;
+    // Return Dynamic Int Address
+    return a;
+    // when i return address of static array &a i get null in output. because address of a automatic remove the static memory address. when i use it must insure fun function have 2 pointer(**);
+    // return &a;
+}
 int main()
 {
-    
+    int *a = fun();
+    cout << *a << endl;
     return 0;
 }
