cedbd86c7f1ee5c4ff1ec664d089c23164b25258
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-30 01:40:07 +0600

    Add Dynamic Array

--- Module-02/Dynamic_Array.cpp
+++ Module-02/Dynamic_Array.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main()
 {
-    
+    // static array
+    // int a[5];
+
+    // dynamic array
+    // int *a = new int[5];
+
+    // dynamic array input and output
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    //  output array
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+
     return 0;
 }
