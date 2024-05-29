4022038ccca3840ad1889f404833cebe0ffc8305
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-30 01:56:29 +0600

    Function With Dynamic Array

---
+++ Module-02/Return_Dynamic_Array_Function.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int *fun()
+{
+    int *a = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+    return a;
+}
+
+int main()
+{
+
+    int *b = fun();
+    for (int i = 0; i < 5; i++)
+    {
+        cout << b[i] << " ";
+    }
+    return 0;
+}
