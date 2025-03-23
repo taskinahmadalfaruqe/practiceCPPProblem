933fbea9a3a44cc67edc99e6a7782c8434af6cf7
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-23 22:39:55 +0800

    Add Veriabel.

---
+++ ProblemSolving/Problem-22.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Declea A Veriable to Seve this value/
+    float FloatNumber = 567.123;
+    char letter = 'F';
+    int IntNumber = 1000;
+    string message = "Welcome To Chine!";
+    
+    cout << message;
+    cout << IntNumber;
+
+    return 0;
+}
