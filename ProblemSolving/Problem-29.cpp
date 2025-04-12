837f2e08c766229bff54b65c645574969e04e41f
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-04-12 23:57:41 +0800

    ADD New Array Prioblem.

---
+++ ProblemSolving/Problem-29.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int first = INT_MIN, second = INT_MIN;
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == INT_MIN) {
+        cout << "No second largest" << endl;
+    } else {
+        cout << second << endl;
+    }
+
+    return 0;
+}
